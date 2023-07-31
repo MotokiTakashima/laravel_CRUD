6f848b60343d14a60c4cbda6a1f2bbb4fd982dcd
高島幹 <takashimamotoki@takashimamikinoMacBook-Air.local>
2023-07-31 22:58:00 +0900

    dbにデータを投入

--- database/migrations/2023_07_26_233300_create_members_table.php
+++ database/migrations/2023_07_26_233300_create_members_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('members', function (Blueprint $table) {
             $table->id();
+            $table->string('name',20);
+            $table->string('telephone',13)->nullable()->unique();
+            $table->string('email',255)->nullable()->unique();
             $table->timestamps();
         });
     }
